b94542f41b5e835ee31d56de9c45eadb1c84f03e
Prisca Ogu <@chidimma.p.ogu@gmail.com>
2020-08-22 20:13:46 +0100

    wave5

---
+++ create_ndarray.py
@@ -0,0 +1,7 @@
+#Use Broadcasting to create a 4 X 4 ndarray that has its first column full of 1s.\
+#its third column full of 3s etc
+import numpy as np
+
+a = np.array([[1,1,1,1],[2,2,2,2],[3,3,3,3],[4,4,4,4]])
+
+print(a)
